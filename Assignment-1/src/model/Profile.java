9939cf3088d1db1eec00648bd15cdcf387ba0216
Hemin Patel <patel.hemin@northeastern.edu>
2024-09-19 21:33:20 -0400

    Assingment - 1

---
+++ Assignment-1/src/model/Profile.java
@@ -0,0 +1,214 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author hemin
+ */
+public class Profile {
+    
+    String fName;
+    String lName;
+    String email;
+    String phone;
+    String nationality;
+    String annualInc;
+    String telephoneNum;
+    String faxNum;
+    String addLine1;
+    String addLine2;
+    String city;
+    String zip;
+    String dlNumber;
+    String SSN;
+    String solAddress;
+    String occupation;
+    String state;
+    String dlState;
+    String gender;
+    Date birthDate;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getAnnualInc() {
+        return annualInc;
+    }
+
+    public void setAnnualInc(String annualInc) {
+        this.annualInc = annualInc;
+    }
+
+    public String getTelephoneNum() {
+        return telephoneNum;
+    }
+
+    public void setTelephoneNum(String telephoneNum) {
+        this.telephoneNum = telephoneNum;
+    }
+
+    public String getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(String faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getAddLine1() {
+        return addLine1;
+    }
+
+    public void setAddLine1(String addLine1) {
+        this.addLine1 = addLine1;
+    }
+
+    public String getAddLine2() {
+        return addLine2;
+    }
+
+    public void setAddLine2(String addLine2) {
+        this.addLine2 = addLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getDlNumber() {
+        return dlNumber;
+    }
+
+    public void setDlNumber(String dlNumber) {
+        this.dlNumber = dlNumber;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getSolAddress() {
+        return solAddress;
+    }
+
+    public void setSolAddress(String solAddress) {
+        this.solAddress = solAddress;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDlState() {
+        return dlState;
+    }
+
+    public void setDlState(String dlState) {
+        this.dlState = dlState;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public Date getBirthDate() {
+        return birthDate;
+    }
+    
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
+    
+
+    
+    
+  
+    
+    
+    
+
+   
+    
+    
+    
+    
+    
+    
+    
+}
